f4f5b677a1eb350d27994cce084ee6ef85063128
Jordan Williams <jwilliams011@student.butte.edu>
2017-08-23 18:50:51 +0000

    Lab1.1 submission

---
+++ lab11/lab11.cpp
@@ -0,0 +1,11 @@
+Numbers are 1 through 10
+
+computer selects the middle of the maximum number
+if selected number is higher, go 3 numbers higher
+if selected number is higher, go up 2 numbers higher
+else go 1 number higher
+
+if selected number is lower, go 2 numbers lower
+if selected number is higher, go up 1 number
+else go 1 number lower
+
