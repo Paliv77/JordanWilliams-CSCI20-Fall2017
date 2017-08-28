cc03a0a1978b098fcd8cdb023d17e7fc7c034af9
Jordan Williams <jwilliams011@student.butte.edu>
2017-08-28 18:55:43 +0000

    Lab1.1 submission

--- lab11/lab11.cpp
+++ lab11/lab11.cpp
@@ -1,11 +1,7 @@
-Numbers are 1 through 10
-
-computer selects the middle of the maximum number
-if selected number is higher, go 3 numbers higher
-if selected number is higher, go up 2 numbers higher
-else go 1 number higher
-
-if selected number is lower, go 2 numbers lower
-if selected number is higher, go up 1 number
-else go 1 number lower
-
+This is for a simple number guessing game.
+Computer randomly selects a number that's 1-10.
+A user will try to guess the computer selected number that was 1-10.
+The user will start with 10 points.
+If the user selects a number and the computer number is higher, the computer will state "higher"
+If the user selects a number and the computer number is lower, the computer will state "lower"
+Each time the computer says "higher" or "lower", minus 1 point from user score.
