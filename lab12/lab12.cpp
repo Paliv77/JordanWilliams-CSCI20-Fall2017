3d053a6db2dbd2448c423437c5aaf5f0b6b48b6a
Jordan Williams <jwilliams011@student.butte.edu>
2017-08-28 22:59:40 +0000

    Lab 1.2 Commit

---
+++ lab12/lab12.cpp
@@ -0,0 +1,25 @@
+*/ Name: Jordan Williams
+* Class: CSCI 020
+* August 28 2017
+* Desc: ASCII Art
+/*
+
+
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+    cout << "          ___" << " / /" << endl;
+    cout << "         /" << " ." << " __" << " ." << "\\" << endl;
+    cout << "         \\" << "   \\" << "/" << "  /"<< endl;
+    cout << "     '" << " __" << "/" << "       \\" << endl;
+    cout << "       \\" << "-" << "         )" << endl;
+    cout << "        \\" << "________" << "/" << endl;
+    cout << "            |" << " |" << endl;
+    cout << "_______________________" << endl;
+    
+}
---
+++ lab12/lec12.cpp
@@ -0,0 +1,19 @@
+/*Created By: April Browne
+* Created On: 8/22/2016
+*/
+ 
+ #include <iostream> //preprocessor directives
+ using namespace std;
+ 
+ int main() //main function
+ {
+     cout<<"Hello"<<" "<<"World!"<<endl;
+     cout<<"Hello"<<" "<<"Jordan!"<<endl;
+ }
+ 
+ //Identify the following parts of the program:
+ //   preprocessor directives
+ //   main function
+ //   comments
+ //update it so that it is human readable
+ //add a second output line that says "Hello YourName!"
