39e835f27f282a13fa05120d9a9a1eaec7f08c64
Jordan Williams <jwilliams011@student.butte.edu>
2017-09-12 20:58:40 +0000

    Lab 1.6 submission

--- lab16/lab16.cpp
+++ lab16/lab16.cpp
@@ -1,6 +1,6 @@
 /* Name: Jordan Williams
  * Date: 9/12/2017
- *Desc: A program to caculate total stock value.
+ * Desc: A program to caculate total stock value.
  *
 */
 
@@ -17,27 +17,31 @@ struct portfolio {
 
 int main(){
   
-    portfolio a;
+    portfolio a; //creates a new object
     
-    cout << "Enter stock name: " << endl;
+    cout << "Enter stock name: " << endl; //Asks for input and stores it in the object
     cin >> a.stock_name;
-    //debug cout << a.stock_name << endl;
-    
+
     cout << "Enter sector: " << endl;
     cin >> a.sector;
-    //debug cout << a.sector;
 
     cout << "What's the current share price?" << endl;
     cin >> a.share_price;
-    //debug cout << a.share_price << endl;
-    
+
     cout << "How many shares do you own?" << endl;
     cin >> a.shares;
-    //debug cout << a.shares << endl;
     
+    //the print out of the end result
     cout << "Stock Name " << "No of Shares " << "Current Value " << "Total Value" << endl;
     cout << "    "<< a.stock_name << "           " << a.shares << "          " << a.share_price << "            " << a.share_price * a.shares << endl;
     cout << "Total Portfolio: " << "$" << a.share_price * a.shares << endl;
 
    return 0; 
 }
+
+/* DEBUG SET
+cout << a.stock_name << endl;
+cout << a.sector;
+cout << a.share_price << endl;
+cout << a.shares << endl;   
+*/
