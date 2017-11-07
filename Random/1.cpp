3cc2b2b294fbe5b413467eb1ffdfb6328fd848a6
Jordan Williams <jwilliams011@student.butte.edu>
2017-11-07 03:24:15 +0000

    Lab4.1 submission

--- Random/1.cpp
+++ Random/1.cpp
@@ -8,89 +8,213 @@
 
 
 #include <iostream>
-#include <cstdlib> //library has random number generator and arithmetics
-#include <ctime> //library has date and time functions for manipulation
+#include <string>
+
 using namespace std;
 
+class ArrayTools {
+    
+    public:
+    
+    int Find_min(int min,int max); //Determines Min. based on parameters
+    int Find_max(int min,int max); //Determines 
+    int Find_sum();
+    int Num_even();
+    int Num_odd();
+    int Search(int find_number);
+    string ls_sorted();
+    
+    ArrayTools();
+    ArrayTools(int array_one[], int size);
+    void Print() const;
+    
+    private:
+    
+    int array[];
+    int size_;
+    string sorted_;
 
- 
-int spinner () {
-    srand(time(0));
-    int spin = rand() % 7 + 1;
-    return spin;
+    
+};
+
+
+ArrayTools::ArrayTools() {
+    
+    int input = 0;
+    size_ = 10;
+    
+    for (int i = 0; i < size_; i++) {
+        
+        array[i] = input;
+        input++;
+    }
 }
 
 
-int main () {
+ArrayTools::ArrayTools(int array_one[], int size) {
     
-    int spin = spinner ();
-    bool players = 1; //0 for player, 1 for computer
-    int player_tree = 10;
-    int computer_tree = 10;
-  
-    int player_basket = 0;
-    int computer_basket = 0;
+    for (int i = 0; i < size; i++) {
+        
+        array[i] = array_one[i];
+        
+    }
     
+    size_ = size;
+}
 
-    while (player_basket < 10 || computer_basket < 10 ) {
-        if (spin == 1 && players == 0) {
-            cout << "You landed on 1. You took 1 cherry from your tree." << endl;
-            player_tree = player_tree - 1;
-            player_basket = player_basket + 1;
-        }
-        else if (spin == 1 && players == 1) {
-            cout << "Computer landed on 1. The computer took 1 cherry from its tree." << endl;
-            computer_tree = computer_tree - 1;
-            computer_basket = computer_basket + 1;
-            cout << computer_basket;
-        }
-        else if (spin == 2 && players == 0) {
-            player_tree = player_tree - 2;
-            player_basket = player_basket + 2;
-        }
-        else if (spin == 2 && players == 1) {
-            computer_tree = computer_tree - 2;
-            computer_basket = computer_basket + 2;
-        }
-        else if (spin == 3 && players == 0) {
-            player_tree = player_tree - 3;
-            player_basket = player_basket + 3;
-        }
-        else if (spin == 3 && players == 1) {
-            computer_tree = computer_tree - 3;
-            computer_basket = computer_basket + 3;
-        }
-        else if (spin == 4 && players == 0) {
-            player_tree = player_tree - 4;
-            player_basket = player_basket + 4;
-        }
-        else if (spin == 4 && players == 1) {
-            computer_tree = computer_tree - 4;
-            computer_basket = computer_basket + 4;
-        }
-        else if (spin == 5 && players == 0 && player_basket >= 2) {
-            player_tree = player_tree + 2;
-            player_basket = player_basket - 2;
-        }
-        else if (spin == 5 && players == 0 && player_basket == 1) {
-            player_tree = player_tree + 1;
-            player_basket = 0;
-        }
-        else if (spin == 5 && players == 0 && player_basket == 0) {
-                //SKIPS TURN TO NEXT PLAYER
-        }
-        else if (spin == 5 && players == 1 && computer_basket >= 2) {
-            computer_tree = computer_tree + 2;
-            computer_basket = computer_basket - 2;
-        }
-        else if (spin == 5 && players == 1 && computer_basket == 1) {
-            computer_tree = computer_tree + 1;
-            computer_basket = 0;
-        }
-        else if (spin == 5 && players == 1 && player_basket == 0) {
-             //SKIPS TURN TO NEXT PLAYER
+
+void ArrayTools::Print() const {
+    for (int i = 0; i < size_; i++) {
+        cout << array[i] << " ";
+    }
+    cout << endl;
+    return;
+}
+
+int ArrayTools::Find_min(int min,int max) {
+    int finding_min = array[min];
+    for (int i = 0; i < size_; i++) {
+        if(array[min] > array[i]) {
+            finding_min = array[i];
         }
     }
+    return finding_min;
+}
+
+
+
+int ArrayTools::Find_max(int min,int max) {
+    
+    int finding_max = array[min];
+    
+    for (int i = min; min <= max; min++) {
+        
+        if(array[min] > finding_max) {
+            
+            finding_max = array[min];
+        }
+    }
+    
+    return finding_max;
+}
+
+
+int ArrayTools::Find_sum() {
+    
+    int sum = 0;
+    
+    for (int i = 0; i < size_; i++) {
+        
+        sum = sum + array[i];
+    }
+    
+    return sum;
+}
+
+
+int ArrayTools::Num_odd() {
+    
+    int odd_ = 0;
+    
+    for (int i = 0; i < size_; i++) {
+        
+        if((array[i] % 2) == 1) {
+            
+            odd_++;
+        }
+    }
+    
+    return odd_;
+}
+
+
+
+int ArrayTools::Num_even() {
+    
+    int even_ = 0;
+    
+    for (int i = 0; i < size_; i++) {
+        
+        if((array[i] % 2) == 0) {
+            
+            even_++;
+        }
+    }
+    
+    return even_;
+}
+        
+     
+int ArrayTools::Search(int find_number) {
+    
+    int i;
+    
+    for (i = 0; i < size_; i++) {
+        
+        if(array[i] == find_number) {
+            
+            return i;
+        }
+        
+        else {
+            
+            return -1;
+        }
+    }
+}
+
+
+string ArrayTools::ls_sorted() {
+    
+    string sorted_;
+    int max_val = array[0];
+    int sort = 0;
+    int i = 0;
+    
+    for (i = 0; i < size_; i++) {
+        
+        if(array[i] >= max_val) {
+            
+            sort++;
+            
+        }
+        
+    }
+    
+    if (sort >= 9) {
+        
+        sorted_ = "True";
+    }
+    
+    else {
+        
+        sorted_ = "False";
+    }
+
+    return sorted_;
+    
+}
+
+
+int main() {
+    
+    const int SIZE = 10;
+    int myArray[SIZE];
+    
+    for (int i = 0; i < SIZE; i++) {
+        
+        cin >> myArray[i];
+    }
+    
+    ArrayTools a(myArray, SIZE);
+    
+    a.Print();
+    
+    cout << "Min: " << a.Find_min(0,4) << endl;
+    cout << "Max: " << a.Find_max(5,10) << endl;
+    cout << "Sum = " << a.Find_sum() << endl;
+    cout << "Search 10: " << a.Search(10) << endl;
+    cout << "Sorted? " << a.ls_sorted() << endl;
     
     return 0;
 }
